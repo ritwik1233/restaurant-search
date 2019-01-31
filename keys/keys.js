0470808d783ecac437e4422263b54a4d580d97a9
ritwik1233 <ritwiksinha25@gmail.com>
2019-02-01 03:34:00 +0530

    Deployment

--- keys/keys.js
+++ keys/keys.js
@@ -1,12 +1,13 @@
-var dev=require('./dev');
-var prod=require('./prod');
+
 var keys={}
 if(process.env.NODE_ENV==='production')
 {
+    var prod=require('./prod');    
     keys=prod
 }
 else
 {
+    var dev=require('./dev');
     keys=dev
 }
 module.exports=keys
